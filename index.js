a8419fa8b5e92625c168fc14e8c98d12eddef556
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2023-12-29 00:17:01 +0000

    updated js script file

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let termperformanceIndex;
+
 // buttons mapped with on HTML 
 const buttonFirst   =   $(".b1");
 const buttonSecond  =   $(".b2");
@@ -22,43 +23,52 @@ const promotionPosition   = $("promotion-position");
 const promotionEnrollment = $("promotion-enrollment");
 
 
-studentInfo = [
+const studentInfo = [
     {
         name: "Christopher Wiafe Debrah",
         age: 27,
         gender: "Male",
-        performanceTerm: [80, 43, 34],
-        performanceSubject: [90, 56, 87],
-        promotingStatus : [firstPerson],
+        performanceTerm: [80, 43, 34 ],
+        performanceSubject: [90, 56, 87 ],
+        nextLevel : [promotedStatus, classPosition, totalStudents],
+    },
+    {
+        name: "Daasebere Dwamena",
+        age: 17,
+        gender: "Male",
+        performanceTerm: [90, 43, 39],
+        performanceSubject: [100, 76, 7],
         nextLevel : [promotedStatus, classPosition, totalStudents],
     }
 ]
 
-function firstPerson (t){
-    // termperformanceIndex = 0
-
+function firstPerson (){
+    let students = studentInfo.length;
+    let academicPerformance = [];
+    let i = 0;
+    while(i < students){
+    // termperformanceIndex = i;
     let totalTermPerformance = 0;
     let averageTermPerformance = 0;
-    // let personOne = studentInfo[termperformanceIndex].performanceTerm;
-    let personOne = studentInfo[t].performanceTerm;
+    
+    let personOne = studentInfo[i].performanceTerm;
     if(personOne.length > 0){
         for(let i = 0; i < personOne.length; i++){
             totalTermPerformance += personOne[i] ;
             averageTermPerformance = Math.floor(totalTermPerformance / personOne.length);
         }
     }
-    console.log("Average term performance for person 1 is: "+ averageTermPerformance);
-    return averageTermPerformance;
+    console.log("Average term performance for person "+ studentInfo[i]["name"] +" is: "+ averageTermPerformance);
+    i++;
+    academicPerformance.push(averageTermPerformance);
+   
 }
 
-function performance(){
 
+console.log(academicPerformance);
 }
 
-console.log("first person mark: "+studentInfo[0].promotedStatus);
-let h = firstPerson(studentInfo[0]);
-console.log(h);
-
+firstPerson();
 
 
 
